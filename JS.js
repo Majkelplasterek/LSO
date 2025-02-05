40868c3658e70f12492567ad0a6fda712672ff8c
Majkelplasterek <146191052+Majkelplasterek@users.noreply.github.com>
2025-02-05 22:38:56 +0100

    Update JS.js

--- JS.js
+++ JS.js
@@ -4,7 +4,11 @@ const PierwszyMobile = document.querySelector(".pierwszyULmobile")
 const Ceremonial = document.querySelector(".childOdd")
 const DrugiMobile = document.querySelector(".drugiULmobile");
 const Odnosniki = document.querySelectorAll(".child, .childSmall")
+const Body = document.querySelector("body");
+const SunMoon = document.querySelector("#sunmoon");
+const Teksty = document.querySelectorAll("h1, p, h2, h3");
 let kat = 0;
+let tryb = 0; //0 jasny 1 ciemny
 document.querySelector("#ikona").addEventListener('click', function(){
     for(let i = 0; i < Listy.length; i++){
         // Listy[i].style.visibility = "visible"
@@ -71,3 +75,31 @@ Odnosniki.forEach(link => {
     });
 });
 
+SunMoon.addEventListener('click', function(){
+
+    if(tryb == 0){
+        tryb = 1;
+        Body.style.backgroundColor = "#292929";
+        SunMoon.src = "moon.png";
+
+        for(let i = 0; i < Teksty.length; i++){
+            if(i == 0){
+                continue;
+            }
+            Teksty[i].style.color = "#ccc";
+        }
+    }
+    else{
+        tryb = 0;
+        Body.style.backgroundColor = "white";
+        SunMoon.src = "sun.png";
+
+        for(let i = 0; i < Teksty.length; i++){
+            if(i == 0){
+                continue;
+            }
+            Teksty[i].style.color = "black";
+        }
+    }
+
+})
